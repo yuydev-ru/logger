4199e98815e000b41fd90186ad778ce24a6f3724
Nikita Rydanov <wrongwayboyy@gmail.com>
2021-07-11 14:09:37 +0400

    Убрал разделители

--- logger.cpp
+++ logger.cpp
@@ -10,37 +10,37 @@ using namespace log;
 void
 FileLogger::error(const std::string &errorMessage)
 {
-    this->destination << "[ERROR] " << errorMessage << std::endl << "===================" << std::endl;
+    this->destination << "[ERROR] " << errorMessage << std::endl;
 }
 
 void
 FileLogger::info(const std::string &infoMessage)
 {
-    this->destination << "[INFO] " << infoMessage << std::endl << "===================" << std::endl;
+    this->destination << "[INFO] " << infoMessage << std::endl;
 }
 
 void
 FileLogger::warning(const std::string &warningMessage)
 {
-    this->destination << "[WARNING] " << warningMessage << std::endl << "===================" << std::endl;
+    this->destination << "[WARNING] " << warningMessage << std::endl;
 }
 
 void
 ConsoleLogger::error(const std::string &errorMessage)
 {
-    this->destination << "[ERROR] " << errorMessage << std::endl << "===================" << std::endl;
+    this->destination << "[ERROR] " << errorMessage << std::endl;
 }
 
 void
 ConsoleLogger::info(const std::string &infoMessage)
 {
-    this->destination << "[INFO] " << infoMessage << std::endl << "===================" << std::endl;
+    this->destination << "[INFO] " << infoMessage << std::endl;
 }
 
 void
 ConsoleLogger::warning(const std::string &warningMessage)
 {
-    this->destination << "[WARNING] " << warningMessage << std::endl << "===================" << std::endl;
+    this->destination << "[WARNING] " << warningMessage << std::endl;
 }
 
 // Constructors
